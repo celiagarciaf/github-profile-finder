54e5ba8bfe417c58b61e5f65135c5c5e2df8321b
Celia Garcia <fgarciacelia@gmail.com>
2025-09-04 12:31:19 +0200

    remove old workflows

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/github-profile-finder/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
